31ffea30fed6e7bf7d5a4c85b963c74bcbd68960
Dawid <93683680+user1527@users.noreply.github.com>
2021-11-04 21:45:59 +0100

    Create virus.bat
    Not dangerous virus. You fire on your own responsibility

---
+++ virus.bat
@@ -0,0 +1,18 @@
+@echo off
+color a
+echo Executing Attack
+timeout /t 2 /nobreak >nul
+echo:
+echo Deleting System Files...
+echo Shutting Down...
+echo Wiping Hard Drive
+echo Gathering Down
+timeout /t 3 /nobreak >nul
+Echo:
+echo Shutting Down
+timeout /t 2 /nobreak >nul
+echo
+color a
+dir /s
+timeout /t 2 /nobreak >nul
+shutdown.exe /h
